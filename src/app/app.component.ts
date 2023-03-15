509b133aebc825e10a8d269c02e494861d138fba
MirkoPiccinno <info@mirkodev.it>
2023-03-15 15:26:14 +0100

    ftm ng base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ftm-ng';
+	// title = 'ftm-ng';
 }
